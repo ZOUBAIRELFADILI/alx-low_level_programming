3ec022f5099bb9812e4c457d62b538c3175493cb
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-02-16 07:44:55 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+int main(void)
+{
+	const char message[]="and that piece of art is useful\" - Dora Korpur, 2025-10-19\n";
+	write(STDERR_FILENO,message, sizeof(message) - 1);
+	return (1);
+}
