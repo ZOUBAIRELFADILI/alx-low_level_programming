fa3829f5fc246a1372809b822239f2d8477da9fe
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-02-16 06:56:36 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
