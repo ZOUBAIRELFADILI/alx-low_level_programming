0a7fc6d9522fe2b23dc2accc6475d916d92a2592
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-02-22 14:07:23 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,16 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j=0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 			if (j == 0)
 			{
-				printf("%d", result);
+				printf("%2d", result);
 			}
 			else
 			{
-				printf(", %d", result);
+				printf(", %2d", result);
 			}
 		}
 		printf("\n");
