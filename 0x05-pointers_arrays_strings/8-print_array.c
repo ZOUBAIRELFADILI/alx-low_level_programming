44289811c96582a0664634eddf4ce60b07f2f922
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-02-27 15:06:52 -0800

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		putchar("%d", *(a + i));
+		printf("%d", *(a + i));
 		if(i != n - 1)
-			putchar(", ");
+			printf(", ");
 	}
 
-	putchar("\n");
+	printf("\n");
 }
