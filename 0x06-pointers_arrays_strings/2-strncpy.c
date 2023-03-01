b16c90c24da2eb4b073f8d2191042f53183cd527
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-01 16:33:41 +0100

    Task2_strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,18 +7,20 @@
  *
  * Return: A pointer to the resulting string dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+
 	/* Copy up to n byets of src to dest */
-	for (i= 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
 	/* Pad dest with null bytes if necessary */
-	for (i = 0; i < n; i++){
+	for (; i < n; i++){
 		dest[i] = '\0';
 	}
 
