2dcdbc17c52cc85a4aa9678f1b3116ccf1da4fd5
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-02 16:20:32 +0100

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to modify
+ *
+ * Return: A pointer to the modified string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		switch (str[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '\"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+				break;
+			default:
+				break;
+		}
+	}
+
+	return (str);
+}
