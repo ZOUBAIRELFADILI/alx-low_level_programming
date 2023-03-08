71912e5a1ec596695b5ca90d36e5e2599833fc90
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-08 13:21:55 -0800

    Task3_factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,19 +6,23 @@
  *
  * Return: the factorial of n, or -1 if n is less than 0
  */
-int factorial(int n)
+int factorial_helper(int n, int acc)
 {
-	int result = 1;
-
 	if (n < 0)
 		return -1;
-	if (n == 0)
-		return 1;
-	while (n > 0)
-	{
-		result *= n;
-		n--;
-	}
 
-	return (result);
+	if (n == 0)
+		return acc;
+	return factorial_helper(n - 1, n * acc);
+}
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to calculate the factorial of
+ *
+ * Return: the factorial of n, or -1 if n is less than 0
+ */
+int factorial(int n)
+{
+	return factorial_helper(n, 1);
 }
