8109e3e12d2ee387d8ea69e917c9cb1f50c2f571
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-08 13:26:20 -0800

    Task3_factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 /**
  * factorial - returns the factorial of a given number
+ * factorial_helper - recursive helper function for factorial
  * @n: the number to calculate the factorial of
- *
+ * @acc: the accumulator value for the calculation
  * Return: the factorial of n, or -1 if n is less than 0
  */
 int factorial_helper(int n, int acc)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 
 	if (n == 0)
-		return acc;
-	return factorial_helper(n - 1, n * acc);
+		return (acc);
+	return (factorial_helper(n - 1, n * acc));
 }
 
 /**
@@ -24,5 +25,5 @@ int factorial_helper(int n, int acc)
  */
 int factorial(int n)
 {
-	return factorial_helper(n, 1);
+	return (factorial_helper(n, 1));
 }
