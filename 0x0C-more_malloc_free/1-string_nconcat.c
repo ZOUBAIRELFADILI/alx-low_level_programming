63298979898be50372ddf114eb0dc5229e2fc4bc
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-16 10:18:30 -0700

    Task1_ConcatenatesTwoStrings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * string_nconcat - Concatentes two strings
+ * @s1: First string
+ * @s2: Second string 
+ * @n: Maximum number of bytes of s2 to concatebate
+ *
+ * Return: Pointer to the newly allocateed space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int len1 = 0, len2 = 0, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+	str = malloc(sizeof(char) * (len + n + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		str[i] = s2[j];
+	str[i] = '\0';
+	return (str);
+}
