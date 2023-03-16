38db3b7bf3cb4450ed9a5770e5029a4bbeacd01b
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-16 09:41:08 -0700

    Task0_AllocatesMemoryUsing_malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size of memory to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ * Exit with status value of 98 if malloc fails
+ */
+void *malloc_checked(usigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(usigned int b);
+
+#endif /* MAIN_H */
