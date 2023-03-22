2cf604b034696e6656850586e6d55894ba21a465
ZOUBAIRELFADILI <elfadili.zoubair@gmail.com>
2023-03-22 04:16:57 -0700

    Task0_PrintName

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer that takes a char pointer as argument
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
